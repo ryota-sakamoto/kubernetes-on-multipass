dc94f3b1b66f5dff2a446965b618cdb80c638c60
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2024-06-16 19:31:43 +0900

    add debug flag

--- cmd/kom/main.go
+++ cmd/kom/main.go
@@ -7,17 +7,6 @@ import (
 	"github.com/ryota-sakamoto/kubernetes-on-multipass/pkg/cmd"
 )
 
-func init() {
-	logLevel := slog.LevelInfo
-	if l, b := os.LookupEnv("LOG_LEVEL"); b {
-		logLevel.UnmarshalText([]byte(l))
-	}
-
-	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
-	})))
-}
-
 func main() {
 	if err := cmd.Execute(); err != nil {
 		slog.Error(err.Error())
--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -12,8 +15,22 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logLevel := slog.LevelInfo
+		if b, _ := cmd.Flags().GetBool("debug"); b {
+			logLevel = slog.LevelDebug
+		}
+
+		slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: logLevel,
+		})))
+	},
 }
 
 func Execute() error {
 	return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("debug", "", false, "Enable debug mode")
 }
